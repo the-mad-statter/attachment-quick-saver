e283f8cc54526c571aaf91b0f442626338c5d9e2
Matthew Schuelke <matthew.schuelke@slu.edu>
2019-01-21 17:32:43 -0600

    Add project files.

---
+++ Attachment Quick Saver/Ribbon1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Attachment_Quick_Saver
+{
+    partial class Ribbon1 : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon1()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Ribbon1));
+            this.tabAttachmentSaver = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.button1 = this.Factory.CreateRibbonButton();
+            this.button2 = this.Factory.CreateRibbonButton();
+            this.tabAttachmentSaver.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabAttachmentSaver
+            // 
+            this.tabAttachmentSaver.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tabAttachmentSaver.Groups.Add(this.group1);
+            this.tabAttachmentSaver.Label = "TabAddIns";
+            this.tabAttachmentSaver.Name = "tabAttachmentSaver";
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.button1);
+            this.group1.Items.Add(this.button2);
+            this.group1.Label = "Save Attachment";
+            this.group1.Name = "group1";
+            // 
+            // button1
+            // 
+            this.button1.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Label = "MATH";
+            this.button1.Name = "button1";
+            this.button1.ShowImage = true;
+            this.button1.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Label = "Desktop";
+            this.button2.Name = "button2";
+            this.button2.ShowImage = true;
+            this.button2.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button2_Click);
+            // 
+            // Ribbon1
+            // 
+            this.Name = "Ribbon1";
+            this.RibbonType = "Microsoft.Outlook.Explorer";
+            this.Tabs.Add(this.tabAttachmentSaver);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.tabAttachmentSaver.ResumeLayout(false);
+            this.tabAttachmentSaver.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tabAttachmentSaver;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button2;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon1 Ribbon1
+        {
+            get { return this.GetRibbon<Ribbon1>(); }
+        }
+    }
+}
---
+++ Attachment Quick Saver/Ribbon1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using Microsoft.Office.Interop.Outlook;
+using System.Windows.Forms;
+
+namespace Attachment_Quick_Saver
+{
+    public partial class Ribbon1
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void saveAttachmentAsFile(Attachment attachment, string f)
+        {
+            string directory = System.IO.Path.GetDirectoryName(f); // full directory
+            string filename = System.IO.Path.GetFileNameWithoutExtension(f);
+            string extension = System.IO.Path.GetExtension(f); //.ext
+            
+            int i = 1;
+            while (System.IO.File.Exists(f))
+            {
+                string newfilename = string.Format("{0}-{1}{2}", filename, i, extension);
+                f = System.IO.Path.Combine(directory, newfilename);
+                i++;
+            }
+
+            try
+            {
+                attachment.SaveAsFile(f);
+            }
+            catch (System.Exception e)
+            {
+                MessageBox.Show("Error saving attachment to " + f + Environment.NewLine + e.Message);
+            }
+        }
+
+        private void saveAttachmentsToDirectory(RibbonControlEventArgs e, string directory)
+        {
+            var explorer = (e.Control.Context as Explorer);
+            var selection = explorer.Selection;
+            if(selection.Application.ActiveExplorer().Selection.Count > 0)
+            {
+                Object selObject = selection.Application.ActiveExplorer().Selection[1];
+                if(selObject is Microsoft.Office.Interop.Outlook.MailItem)
+                {
+                    Microsoft.Office.Interop.Outlook.MailItem mailitem = (selObject as Microsoft.Office.Interop.Outlook.MailItem);
+                    if (mailitem.Attachments.Count > 0)
+                    {
+                        foreach (Attachment attachment in mailitem.Attachments)
+                        {
+                            saveAttachmentAsFile(attachment, directory + mailitem.SentOn.ToString("yyyy-MM-dd") + ".pdf");
+                        }
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            saveAttachmentsToDirectory(e, Properties.Settings.Default.button1_path);
+        }
+
+        private void button2_Click(object sender, RibbonControlEventArgs e)
+        {
+            saveAttachmentsToDirectory(e, Properties.Settings.Default.button2_path);
+        }
+    }
+}
